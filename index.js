02873e2054d287ed8f5e96811f5fed6da534e433
Lambert Nnadi <nnadi406@gmail.com>
2022-10-16 05:58:16 +0100

    new commit

--- index.js
+++ index.js
@@ -6,50 +6,49 @@ var http = require('http').createServer(app);
 // var io = require('socket.io')(http, {allowEIO3: true, allowEIO4: true, serveClient: true});
 
 var io = require('socket.io')(http, {
-  allowEIO3: true,
-  allowEIO4: true,
-  serveClient: true,
-  cors: { origin: '*'}
+    allowEIO3: true,
+    allowEIO4: true,
+    serveClient: true,
+    cors: { origin: '*' }
 });
 
-http.listen(3000, function(){ console.log('listening on *:3000');});
+http.listen(3000, function() { console.log('listening on *:3000'); });
 
 var serverID = 'undefined';
-io.on('connection', function (socket){
+io.on('connection', function(socket) {
     console.log('a user connected: ' + socket.id + " (server: " + serverID + " )");
     //register the server id, received the command from unity
-    socket.on('RegServerId', function (data){
+    socket.on('RegServerId', function(data) {
         serverID = socket.id;
         console.log('reg server id : ' + serverID);
     });
 
-    socket.on('disconnect', function(){
-        if (serverID == socket.id)
-        {
-           serverID = 'undefined';
-           console.log('removed Server: ' + socket.id);
-        }
-        else
-        {
-           console.log('user disconnected: ' + socket.id);
+    socket.on('disconnect', function() {
+        if (serverID == socket.id) {
+            serverID = 'undefined';
+            console.log('removed Server: ' + socket.id);
+        } else {
+            console.log('user disconnected: ' + socket.id);
         }
     });
 
-    socket.on('OnReceiveData', function (data){
-        if (serverID != 'undefined')
-        {
-            switch(data.EmitType)
-            {
+    socket.on('OnReceiveData', function(data) {
+        if (serverID != 'undefined') {
+            switch (data.EmitType) {
                 //emit type: all;
-                case 0: io.emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte }); break;
-                //emit type: server;
-                case 1: io.to(serverID).emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte }); break;
-                //emit type: others;
-                case 2: socket.broadcast.emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte }); break;
+                case 0:
+                    io.emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte });
+                    break;
+                    //emit type: server;
+                case 1:
+                    io.to(serverID).emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte });
+                    break;
+                    //emit type: others;
+                case 2:
+                    socket.broadcast.emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte });
+                    break;
             }
-        }
-        else
-        {
+        } else {
             console.log('cannot find any active server');
         }
     });
